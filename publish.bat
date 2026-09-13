1f0708ce130e39d51c1b74a3c58fed33ef376f05
Vachaspati Om <aghora.shmashan.prabkhu@gmail.com>
2026-09-13 15:50:30 +0200

    Update catalog

--- publish.bat
+++ publish.bat
@@ -4,32 +4,30 @@ cd /d E:\BibCatalog
 
 if exist publish_log.txt del publish_log.txt
 
-echo ============================================================ >> publish_log.txt
-echo   Публикация каталога библиотеки ШПСА >> publish_log.txt
-echo   %date% %time% >> publish_log.txt
-echo ============================================================ >> publish_log.txt
-echo. >> publish_log.txt
-
 echo ============================================================
-echo   Публикация каталога библиотеки ШПСА
+echo   Publishing library catalog SHPSA
+echo   %date% %time%
 echo ============================================================
 echo.
 
-echo [1/4] Пересборка index.html из Каталог.xlsx ...
-python update_catalog_from_xlsx.py Каталог.xlsx catalog_template.html index.html >> publish_log.txt 2>&1
+echo [1/4] Rebuilding index.html ...
+python update_catalog_from_xlsx.py Каталог.xlsx catalog_template.html index.html
 
+echo.
 echo [2/4] git add ...
-git add -A >> publish_log.txt 2>&1
+git add -A
 
+echo.
 echo [3/4] git commit ...
-git commit -m "Обновление каталога" >> publish_log.txt 2>&1
+git commit -m "Update catalog"
 
+echo.
 echo [4/4] git push ...
-git push >> publish_log.txt 2>&1
+git push
 
 echo.
 echo ============================================================
-echo   Готово! Сайт обновится через 1-2 минуты:
+echo   Done! Site will update in 1-2 minutes:
 echo   https://biblioteka-shpsa.pages.dev
 echo ============================================================
 echo.
